caf2c94bd29e7299ef20114daff40c34a405cbd2
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2025-02-03 14:10:25 +0300

    lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
+"""
+lockbox module
+"""
+
+
 def canUnlockAll(boxes):
     """
-    Determines if all boxes can be opened.
-
-    Args:
-        boxes (list): List of lists, where each inner list represents keys in a box.
-
-    Returns:
-        bool: True if all boxes can be opened, otherwise False.
+    function to check whether all the boxes have been opened or can they be
+    opened with the provided key
     """
-    if not isinstance(boxes, list) or not all(isinstance(box, list) for box in boxes):
-        return False  # Ensure valid input format
 
     n = len(boxes)
-    visited = set([0])  # Start with the first box unlocked
-    stack = [0]  # Stack for DFS traversal
-
-    while stack:
-        current_box = stack.pop()
-        for key in boxes[current_box]:
-            if isinstance(key, int) and 0 <= key < n and key not in visited:
-                visited.add(key)
-                stack.append(key)
-
-    return len(visited) == n
+    visited = set([0])
+    not_visited = set(boxes[0]).difference(set([0]))
+    while len(not_visited) > 0:
+        box = not_visited.pop()
+        if not box or box >= n or box < 0:
+            continue
+        if box not in visited:
+            not_visited = not_visited.union(boxes[box])
+            visited.add(box)
+    return n == len(visited)
